54ca4a6172ea22592fe988646c0e0846a06b2222
Alejandro Romero <romero.m.alejandro@gmail.com>
2018-04-10 02:24:37 +0000

    Se cambian los textos de la home y de la seccion informacion

--- files/modules/information/information.php
+++ files/modules/information/information.php
@@ -8,23 +8,31 @@
 	<?php include("../../includes/inc.top.php"); ?>
 	<div id="Content">
         <div class="LightGray" style="font-style:italic;font-family: 'sans-serif'!important;font-size:20px;width:60%;margin:0 auto;">
-        <p>Estimados colegas:</p>
-        <p>El Campus cuenta con los siguientes foros: 1&deg;, 2&deg; y 3&deg; a&ntilde;o; 
-        Odontoestomatolog&iacute;a, Farmacia y Graduados. A&uacute;n no se conform&oacute; el Foro de Veterinaria.</p>
-        <p>Los siguientes docentes est&aacute;n a cargo de:<br>
-        1&deg; a&ntilde;o: Dra. Astrid Motura<br>
-        1&deg; a&ntilde;o (Mar del Plata): Dra. Silvia Mercado<br>
-        2&deg; a&ntilde;o: Dra. Elena Figari<br>
-        3&deg; a&ntilde;o: Dr. Adolfo Campanelli<br>
-        Graduados: Dra. M&oacute;nica Prunell<br>
-        Odontoestomatolog&iacute;a: Dra. Silvia Palacios<br>
-        Farmacia: Dra. Ana Mar&iacute;a Fern&aacute;ndez<br>
+        <h3 style="text-align: center;"><strong>¡Bienvenidos al Campus Virtual de la Escuela de Posgrado de la Asociación Médica Homeopática Argentina!</strong></h3>
+        <p>El <b>Campus virtual</b> está especialmente diseñado para nuestros alumnos y socios, a fin de generar un intercambio permanente y productivo. En él encontrarán información, artículos de interés, ateneos y novedades relacionadas con la homeopatía.</p>
+        <p>Van a encontrar los foros de <b>1°, 2° y 3° año; Odontología y estomatología, Farmacia y Graduados</b>. Los foros están a disposición de ustedes para estar conectados siempre.</p>
+        <p>Los docentes a cargo son:<br>
+        <ul style="list-style:outside!important;">
+            <li><strong>Foro 1° año:</strong> Dra. Astrid Motura</li>
+            <li><strong>Foro 2° año:</strong> Dra. Elena Figari</li>
+            <li><strong>Foro 3° año:</strong> Dr. Lucas Yraola</li>
+            <li><strong>Foro Odontología y Estomatología Homeopáticas:</strong> Dra. Marina Galán</li>
+            <li><strong>Foro Farmacia:</strong> Dra. Ana María Fernández</li>
+            <li><strong>Foro Graduados:</strong> Dr. Adolfo Campanelli</li>
+            <li><strong>Foro Mar del Plata:</strong> Dra. Silvia Mercado</li>
+        </ul>
+        </p>
+        <p>Diríganse a ellos por este medio para sugerencias, inquietudes, comentarios o preguntas. El sitio está construido para el intercambio dinámico y activo, disponible en cualquier día y horario.</p>
+        <p>Si todavía no cuentan con el <strong>usuario</strong> y la <strong>contraseña</strong>, los alumnos pueden comunicarse con la Secretaría de la Escuela: <a href="https://mail.google.com/mail/?view=cm&fs=1&to=escuela@amha.org.ar" target="_blank" style="color:#0A0!important;"><strong>escuela@amha.org.ar</strong></a>  y  los  Socios: <a href="https://mail.google.com/mail/?view=cm&fs=1&to=amhasocios@gmail.com" target="_blank" style="color:#0A0!important;"><strong>amhasocios@gmail.com</strong></a></p>
         <br>
-        <p>Dir&iacute;ganse a ellos por este medio para sugerencias, inquietudes, comentarios o preguntas. El Campus Virtual est&aacute; construido para el intercambio din&aacute;mico y activo, disponible en cualquier d&iacute;a y horario.</p>
-        <p>La Secretar&iacute;a de la Escuela dar&aacute; a cada alumno un usuario y una contrase&ntilde;a para poder acceder a los foros. Si todav&iacute;a no los han recibido, solic&iacute;tenselos a nuestra secretaria, Sra. Martha Lugo.</p>
-        <p>Los graduados recibir&aacute;n esta informaci&oacute;n por e-mail y en el caso que no les haya llegado, cont&aacute;ctense con la secretaria de Socios, Sra. Graciela M&eacute;ndez.</p><br> 
-        <p>Les agradecemos la participaci&oacute;n.</p>
-        <p style="text-align:right;">Comit&eacute; Directivo Escuela de Posgrado</p>
+        <p><strong>Nuestra Escuela</strong> brinda un lugar en el que se dicta a diario 1 clase por la mañana y otra por la tarde todos los días, sobre materia médica, doctrina, filosofía homeopática y repertorio y se asiste <strong>a pacientes</strong> en forma presencial (como en las Guardias o Consultorios Externos hospitalarios) para que quien estudia adquiera conocimiento guiado por los profesionales que forman nuestro Cuerpo Docente y puedan aprender <strong>cómo se registran los síntomas</strong> que son útiles para una Historia Clínica Homeopática.</p>
+        <p>Es un lugar al que usted podrá <strong>concurrir libremente TODOS LOS DÍAS</strong> que desee o pueda según sus horarios, <strong>ASISTIR A CADA CLASE DE LA MAÑANA</strong> o <strong>DE LA TARDE TODOS LOS DÍAS</strong> de la semana, <strong>TANTAS VECES COMO USTED DESEE</strong> o pueda (más de una vez por semana).</p>
+        <p>La <strong>Escuela</strong> ofrece guiarlo en cada paso que usted necesite en este paradigma distinto del que aprendimos en nuestras Facultades de Medicina. Para ello contamos con un Cuerpo Docente formado por Profesionales con experiencia. Ellos le irán aclarando cada una de las dudas que puedan presentarse. Le mostrarán lo que es necesario recordar de nuestra Doctrina Homeopática y de cada uno de los medicamentos "sin drogas" que forman el arsenal terapéutico homeopático. La homeopatía fue sin dudas, la primera medicina que probó sus medicinas en el hombre sano para determinar sus acciones de en el enfermo y eliminar sus efectos tóxicos.</p>
+        <p>Podrá recibir clases de los Profesionales experimentados que forman nuestro Cuerpo Docente. Con ellos podrá construir su propia formación homeopática y, si lo desea, aprender a atender pacientes en nuestros Consultorios Externos o ingresar a nuestra Carrera Docente.</p>
+        <p><strong>Somos  la única escuela que puede ofrecerle APRENDIZAJE CON PACIENTES TODOS LOS DÍAS de la semana, MAÑANA y TARDE y hacer de usted un Médico Homeópata con intensa formación clínica.</strong></p>
+        <br>
+        <p>Por todo esto, los esperamos para compartir con nosotros la alegría del conocimiento.</p>
+        <br>
     </div>
     </div>
 </body>
