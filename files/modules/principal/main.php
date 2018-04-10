8a59d389f7f00ae707d3c3511ce2970c6e840e2e
Alejandro Romero <romero.m.alejandro@gmail.com>
2018-04-10 02:33:06 +0000

    Se cambian los textos de la home y de la seccion informacion

--- files/modules/principal/main.php
+++ files/modules/principal/main.php
@@ -15,12 +15,8 @@
 			<p style="text-align: justify;">El camino de los semejantes, después de Hipócrates, fue seguido por mucho médicos de los que se tiene, de algunos de ellos, someras nociones y de la gran mayoría, el absoluto desconocimiento. Hasta el siglo XVII, en el que un médico alemán, desilusionado de esa práctica médica oficial, que por sus métodos y consecuencias generó aquello de que “es peor el remedio que la enfermedad”, retoma el viejo aforismo hipocrático “similia similibus curatur” y después de incansables experimentos y comprobaciones, de una lucha extenuante ante propios y ajenos, pone en marcha el germen de ese arte maravilloso de curar que es la Homeopatía Unicista.<br>Este hombre fue Samuel Hahnemann.</p>
 			<p style="text-align: justify;">Y ustedes comienzan hoy a transitar su camino. Comienzan a deshilvanar ese entramado fascinante que al final del ovillo les ofrece otra medicina, que es la del enfermo, ese ser humano que padece los desequilibrios de su principio vital y lo exterioriza a través de una signo-sintomatología que muchas veces se escapa a la visión de la semiología clínica y a la que hay que buscar, en no pocas ocasiones, con los ojos de la comprensión y el compromiso. y de la paciencia… y del calor humano.</p>
 			<p style="text-align: justify;">Bienvenidos al mundo médico hipocrático del  “similia similibus curatur” que Hahnemann, como un alquimista mágico, transformó, modificó y amplió, con el magnífico poder de su intelecto y de su incansable afán de experimentación. luchando durante toda su vida con enormes adversidades en  “similia similibus curatur”.</p>
-			
 			<p style="text-align: justify;">Bienvenidos a la decana de las escuela de homeopatía de la Argentina.</p>
 			<p style="text-align: justify;">Este año cumplimos 85 años de lucha incansable en pos de la perfección del cumplimiento de los principios hahnemannianos.<br>Bienvenidos a la Homeopatía. Los estábamos esperando.</p>
-			
-			<br />
-			<p style="text-align: justify;">&#160;Por todo esto, los esperamos para compartir con nosotros la alegr&iacute;a del conocimiento.</p>
 			<br />
 			<p style="text-align: right;">Dr. Adolfo Campanelli<br/>Director de la Escuela de Posgrado<br/><a href="http://www.amha.org.ar" target="_blank" ><b>A.M.H.A.</b></a></p>
 		</div>
