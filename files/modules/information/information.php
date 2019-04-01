82d1579bf721c4a03620fd3f8a28f7c464463966
Alejandro Romero <romero.m.alejando@gmail.com>
2019-04-01 20:27:01 -0300

    Se cambia el home y la pantalla de informacion

--- files/modules/information/information.php
+++ files/modules/information/information.php
@@ -8,7 +8,7 @@
 	<?php include("../../includes/inc.top.php"); ?>
 	<div id="Content">
         <div class="LightGray" style="font-style:italic;font-family: 'sans-serif'!important;font-size:20px;width:60%;margin:0 auto;">
-        <h3 style="text-align: center;"><strong>¡Bienvenidos al Campus Virtual de la Escuela de Posgrado de la Asociación Médica Homeopática Argentina!</strong></h3>
+        <!-- <h3 style="text-align: center;"><strong>¡Bienvenidos al Campus Virtual de la Escuela de Posgrado de la Asociación Médica Homeopática Argentina!</strong></h3> -->
         <p>El <b>Campus virtual</b> está especialmente diseñado para nuestros alumnos y socios, a fin de generar un intercambio permanente y productivo. En él encontrarán información, artículos de interés, ateneos y novedades relacionadas con la homeopatía.</p>
         <p>Van a encontrar los foros de <b>1°, 2° y 3° año; Odontología y estomatología, Farmacia y Graduados</b>. Los foros están a disposición de ustedes para estar conectados siempre.</p>
         <p>Los docentes a cargo son:<br>
@@ -25,14 +25,14 @@
         <p>Diríganse a ellos por este medio para sugerencias, inquietudes, comentarios o preguntas. El sitio está construido para el intercambio dinámico y activo, disponible en cualquier día y horario.</p>
         <p>Si todavía no cuentan con el <strong>usuario</strong> y la <strong>contraseña</strong>, los alumnos pueden comunicarse con la Secretaría de la Escuela: <a href="https://mail.google.com/mail/?view=cm&fs=1&to=escuela@amha.org.ar" target="_blank" style="color:#0A0!important;"><strong>escuela@amha.org.ar</strong></a>  y  los  Socios: <a href="https://mail.google.com/mail/?view=cm&fs=1&to=amhasocios@gmail.com" target="_blank" style="color:#0A0!important;"><strong>amhasocios@gmail.com</strong></a></p>
         <br>
-        <p><strong>Nuestra Escuela</strong> brinda un lugar en el que se dicta a diario 1 clase por la mañana y otra por la tarde todos los días, sobre materia médica, doctrina, filosofía homeopática y repertorio y se asiste <strong>a pacientes</strong> en forma presencial (como en las Guardias o Consultorios Externos hospitalarios) para que quien estudia adquiera conocimiento guiado por los profesionales que forman nuestro Cuerpo Docente y puedan aprender <strong>cómo se registran los síntomas</strong> que son útiles para una Historia Clínica Homeopática.</p>
+        <!-- <p><strong>Nuestra Escuela</strong> brinda un lugar en el que se dicta a diario 1 clase por la mañana y otra por la tarde todos los días, sobre materia médica, doctrina, filosofía homeopática y repertorio y se asiste <strong>a pacientes</strong> en forma presencial (como en las Guardias o Consultorios Externos hospitalarios) para que quien estudia adquiera conocimiento guiado por los profesionales que forman nuestro Cuerpo Docente y puedan aprender <strong>cómo se registran los síntomas</strong> que son útiles para una Historia Clínica Homeopática.</p>
         <p>Es un lugar al que usted podrá <strong>concurrir libremente TODOS LOS DÍAS</strong> que desee o pueda según sus horarios, <strong>ASISTIR A CADA CLASE DE LA MAÑANA</strong> o <strong>DE LA TARDE TODOS LOS DÍAS</strong> de la semana, <strong>TANTAS VECES COMO USTED DESEE</strong> o pueda (más de una vez por semana).</p>
         <p>La <strong>Escuela</strong> ofrece guiarlo en cada paso que usted necesite en este paradigma distinto del que aprendimos en nuestras Facultades de Medicina. Para ello contamos con un Cuerpo Docente formado por Profesionales con experiencia. Ellos le irán aclarando cada una de las dudas que puedan presentarse. Le mostrarán lo que es necesario recordar de nuestra Doctrina Homeopática y de cada uno de los medicamentos "sin drogas" que forman el arsenal terapéutico homeopático. La homeopatía fue sin dudas, la primera medicina que probó sus medicinas en el hombre sano para determinar sus acciones de en el enfermo y eliminar sus efectos tóxicos.</p>
         <p>Podrá recibir clases de los Profesionales experimentados que forman nuestro Cuerpo Docente. Con ellos podrá construir su propia formación homeopática y, si lo desea, aprender a atender pacientes en nuestros Consultorios Externos o ingresar a nuestra Carrera Docente.</p>
         <p><strong>Somos  la única escuela que puede ofrecerle APRENDIZAJE CON PACIENTES TODOS LOS DÍAS de la semana, MAÑANA y TARDE y hacer de usted un Médico Homeópata con intensa formación clínica.</strong></p>
         <br>
         <p>Por todo esto, los esperamos para compartir con nosotros la alegría del conocimiento.</p>
-        <br>
+        <br> -->
     </div>
     </div>
 </body>
--- files/modules/principal/main.php
+++ files/modules/principal/main.php
@@ -9,16 +9,36 @@
 	<div id="Content" style="">
 		<!--<div class="Center resize"><img src="../../../skin/images/body/logos/campus_logo3.png" width="200" height="189" /></div>-->
 		<div class="DarkBlue" style="font-style:italic;font-family: 'sans-serif'!important;font-size:20px;width:60%;margin:0 auto;color:#dbe8fc!important;">
-			
-			<p style="text-align: justify;">La homeopatía es el Arte y la ciencia de curar por medio de los semejantes.</p>
-			<p style="text-align: justify;">El padre de la Medicina, Hipócrates, curaba por los contrarios y por los semejantes. Galeano Tomó el camino de los contrarios y generó una corriente médica que con todas las modificaciones que fue sufriendo a lo largo de los siglos, desemboca, finalmente en la medicina actual, llamada Oficial.</p>
-			<p style="text-align: justify;">El camino de los semejantes, después de Hipócrates, fue seguido por mucho médicos de los que se tiene, de algunos de ellos, someras nociones y de la gran mayoría, el absoluto desconocimiento. Hasta el siglo XVII, en el que un médico alemán, desilusionado de esa práctica médica oficial, que por sus métodos y consecuencias generó aquello de que “es peor el remedio que la enfermedad”, retoma el viejo aforismo hipocrático “similia similibus curatur” y después de incansables experimentos y comprobaciones, de una lucha extenuante ante propios y ajenos, pone en marcha el germen de ese arte maravilloso de curar que es la Homeopatía Unicista.<br>Este hombre fue Samuel Hahnemann.</p>
-			<p style="text-align: justify;">Y ustedes comienzan hoy a transitar su camino. Comienzan a deshilvanar ese entramado fascinante que al final del ovillo les ofrece otra medicina, que es la del enfermo, ese ser humano que padece los desequilibrios de su principio vital y lo exterioriza a través de una signo-sintomatología que muchas veces se escapa a la visión de la semiología clínica y a la que hay que buscar, en no pocas ocasiones, con los ojos de la comprensión y el compromiso. y de la paciencia… y del calor humano.</p>
-			<p style="text-align: justify;">Bienvenidos al mundo médico hipocrático del  “similia similibus curatur” que Hahnemann, como un alquimista mágico, transformó, modificó y amplió, con el magnífico poder de su intelecto y de su incansable afán de experimentación. luchando durante toda su vida con enormes adversidades en  “similia similibus curatur”.</p>
-			<p style="text-align: justify;">Bienvenidos a la decana de las escuela de homeopatía de la Argentina.</p>
-			<p style="text-align: justify;">Este año cumplimos 85 años de lucha incansable en pos de la perfección del cumplimiento de los principios hahnemannianos.<br>Bienvenidos a la Homeopatía. Los estábamos esperando.</p>
+
+			<div style="text-align:center;"><h1>¡Bienvenidos al Campus Virtual de la Escuela de Posgrado de la Asociación Médica Homeopática Argentina!</h1></div>
+
+			<p style="text-align:justify;">A los nuevos alumnos los invitamos a comenzar el estudio de la Homeopatía Unicista Hahnemanniana con entusiasmo, curiosidad y espíritu crítico. Como el de su creador, el Dr. Christian Federico Samuel Hahnemann, quién desilusionado de la medicina de su época, retoma el aforismo de Hipócrates “Similia similibus curentur” (lo semejante cura lo semejante), en contraposición a la concepción galénica de curar con los contrarios, que prevalecía en esa época. Hahnemann propone una medicina del Enfermo y de la Individualidad, y no de la enfermedad, buscando el equilibrio de la fuerza vital a través de la utilización de medicamentos semejantes; todos estos, conceptos fundamentales de la Filosofía Homeopática que se desarrollarán a lo largo de los cursos.</p>
+
+			<p style="text-align: justify;">Así como el médico Homeópata acompaña al paciente en su proceso de curación, los docentes de la Asociación estamos dispuestos a guiarlos en este camino que conlleva un cambio de paradigma, inicialmente tan complejo como apasionante. De manera personalizada, los alumnos dispondrán de asesoramiento por el cuerpo docente de las distintas cátedras donde se asiste a los pacientes contando además con un sistema de tutorías, que guiará al alumno tanto en los aspectos clínicos y teóricos, como en la realización de los  trabajos prácticos requeridos. </p>
+			<p style="text-align: justify;">Nuestro Campus virtual  está integrado por los Foros de las distintas carreras, cada uno con un docente coordinador a disposición para intercambiar ideas, aclarar dudas, proponer bibliografía y donde a la vez se tendrá acceso a un choice mensual de autoevaluación realizado por el docente que dictó la clase.</p>
+			<p style="text-align: justify;">Para nuestros alumnos que ya se iniciaron en su formación como homeópatas reafirmamos nuestro compromiso de acompañarlos en el proceso. Y les recordamos que la participación activa es de fundamental importancia a la hora de adquirir no solo los conocimientos académicos, sino  también de fomentar el cambio paradigmático en el concepto filosófico acerca del individuo, tanto en la salud como en la enfermedad; el famoso clic homeopático que nos transforma de simples prescriptores a verdaderos homeópatas. Para alcanzar esto cuentan con las clases teóricas, los ateneos, las actividades de cátedra, los foros del Campus virtual y las tutorías; todas estas, herramientas fundamentales que facilitan el proceso y despiertan las experiencias necesarias para formar conceptos que servirán de base para adquirir conocimientos de manera progresiva. </p>
+			<p style="text-align: justify;">Nuestra Asociación ha cumplido ya sus primeros 85 años, y lejos de sentir los síntomas de la vejez, está más activa que nunca incorporando nuevas carreras, participando en todos los Congresos (nacionales e internacionales) y estando alertas a las nuevas tendencias que surgen en el mundo. Todo esto realizado con el objetivo puesto en el ideal de todo tratamiento homeopático que es “restablecer la salud en forma rápida, suave y permanente, o sea quitar y destruir toda la enfermedad por el camino más corto, más seguro y menos perjudicial, basándose en principios de fácil comprensión”, según expresa el parágrafo 2 del Organón de Hahnemann. </p>
+			<p style="text-align: justify;">Esperamos poder contagiarles el entusiasmo y la admiración por la Homeopatía, tal como lo han hecho  nuestros Maestros con nosotros.</p>
+
+
 			<br />
-			<p style="text-align: right;">Dr. Adolfo Campanelli<br/>Director de la Escuela de Posgrado<br/></p>
+			<table border="0" margin="0" padding="0" style="width:100%;">
+
+					<tr>
+
+							<td style="text-align: right;">Dr. Lucas Yraola<br/>Director de la Escuela de Posgrado</td>
+							<td style="text-align: right;">Dr. Gustavo Martello<br/>Sub Director de la Escuela de Posgrado</td>
+							<td style="text-align: right;">Dra. Astrid Motura<br/>Secretaria de la Escuela de Posgrado</td>
+
+					</tr>
+
+			</table>
+			<br>
+			<!-- <p style="text-align: right;">Dr. Lucas Yraola<br/>Director de la Escuela de Posgrado<br/></p>
+			<br />
+			<p style="text-align: right;">Dr. Gustavo Martello<br/>Sub Director de la Escuela de Posgrado<br/></p>
+			<br />
+			<p style="text-align: right;">Dra. Astrid Motura<br/>Secretaria de la Escuela de Posgrado<br/></p> -->
 		</div>
     </div>
 </body>
